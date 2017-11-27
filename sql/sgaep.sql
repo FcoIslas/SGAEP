98b1b282a04b416e50f66dfadab9463bec1b8301
FcoIslas <33728123+FcoIslas@users.noreply.github.com>
2017-11-26 23:25:29 -0600

    Upload the final sql
    This is the main that I used to program

--- sql/sgaep.sql
+++ sql/sgaep.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.15.10
--- https://www.phpmyadmin.net
+-- version 4.6.6
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Nov 23, 2017 at 03:17 AM
--- Server version: 5.5.56-MariaDB
--- PHP Version: 5.4.16
+-- Servidor: localhost
+-- Tiempo de generación: 27-11-2017 a las 02:55:47
+-- Versión del servidor: 10.1.25-MariaDB
+-- Versión de PHP: 5.6.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,16 +17,16 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `sgaep`
+-- Base de datos: `sgaep`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tableBooleanQuestions`
+-- Estructura de tabla para la tabla `tableBooleanQuestions`
 --
 
-CREATE TABLE IF NOT EXISTS `tableBooleanQuestions` (
+CREATE TABLE `tableBooleanQuestions` (
   `vcIdQuestion` varchar(60) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL COMMENT 'Var char de la pregunta',
   `intParcial` int(60) NOT NULL,
   `ltQuestion` longtext CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
@@ -34,22 +34,26 @@ CREATE TABLE IF NOT EXISTS `tableBooleanQuestions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tableBooleanQuestions`
+-- Volcado de datos para la tabla `tableBooleanQuestions`
 --
 
 INSERT INTO `tableBooleanQuestions` (`vcIdQuestion`, `intParcial`, `ltQuestion`, `boolAnswer`) VALUES
-('149904IAAJ9312011A05', 2, 'ya me cargÃ³ el payaso', 1),
-('213487IAAJ9312011A0IntroduccionalasBasesdeDatos2', 1, 'que sÃ­ ya me cargÃ³ el payaso', 0),
-('335985IAAJ9312011A0CÃ¡lculo Diferencial e Integral I3', 2, 'fco estÃ¡ enfermo', 1),
-('335985IAAJ9312011A0Cálculo Diferencial e Integral I3', 2, 'fco está enfermo', 1);
+('539343IAAJ9312011A0AdmistracionPublicaeInformatica9', 2, 'Â¿SÃ©ptima pregunta booleana?', 0),
+('543374IAAJ9312011A0AdmistracionPublicaeInformatica9', 2, 'Â¿Tercera pregunta booleana?', 1),
+('700956IAAJ9312011A0AdmistracionPublicaeInformatica9', 1, 'Â¿Primer pregunta booleana?', 1),
+('740894IAAJ9312011A0AdmistracionPublicaeInformatica9', 1, 'Â¿Sexta pregunta booleana?', 0),
+('764667IAAJ9312011A0AdmistracionPublicaeInformatica9', 1, 'Â¿Segunda pregunta booleana?', 0),
+('803768IAAJ9312011A0AdmistracionPublicaeInformatica9', 1, 'Â¿Quinta pregunta booleana?', 1),
+('869875IAAJ9312011A0AdmistracionPublicaeInformatica9', 2, 'Â¿Octava Pregunta booleana?', 0),
+('945662IAAJ9312011A0AdmistracionPublicaeInformatica9', 2, 'Â¿Cuarta pregunta booleana?', 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tableMultipleQuestions`
+-- Estructura de tabla para la tabla `tableMultipleQuestions`
 --
 
-CREATE TABLE IF NOT EXISTS `tableMultipleQuestions` (
+CREATE TABLE `tableMultipleQuestions` (
   `vcIdQuestion` varchar(60) NOT NULL,
   `intParcial` int(10) NOT NULL,
   `ltQuestion` longtext NOT NULL,
@@ -61,20 +65,27 @@ CREATE TABLE IF NOT EXISTS `tableMultipleQuestions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Table to keep questions & answers for "multiple" questions';
 
 --
--- Dumping data for table `tableMultipleQuestions`
+-- Volcado de datos para la tabla `tableMultipleQuestions`
 --
 
 INSERT INTO `tableMultipleQuestions` (`vcIdQuestion`, `intParcial`, `ltQuestion`, `ltAnswerA`, `ltAnswerB`, `ltAnswerC`, `ltAnswerD`, `vcCorrectAnswer`) VALUES
-('233585IAAJ9312011A0QuimicaMatematicas2do3', 2, 'tengo psoriasis', 'sÃ­ ', 'a huevo', 'pero por su puesto', 'ja, por pendejo', 'A'),
-('318626IAAJ9312011A0Contabilidad II2', 1, 'tengo psoriasis', 'sí', 'no', 'pinche exagerado', 'ya te cargó el payaso', 'D');
+('116260IAAJ9312011A0AdmistracionPublicaeInformatica9', 2, 'Â¿sexta pregunta mÃºltiple?', '1', '2', '3', '4', 'A'),
+('121525IAAJ9312011A0AdmistracionPublicaeInformatica9', 1, 'Â¿Quinta pregunta mÃºltiple=', 'uno', 'dos', 'tres', 'cuatro', 'D'),
+('212085IAAJ9312011A0AdmistracionPublicaeInformatica9', 2, 'Â¿segunda pregunta mÃºltiple?', 'uno', 'dos', 'tres', 'cuatro', 'B'),
+('416905IAAJ9312011A0AdmistracionPublicaeInformatica9', 1, 'Â¿Primera pregunta mÃºltiple?', 'sÃ­', 'dos', 'tres', 'cuatro', 'A'),
+('447985IAAJ9312011A0AdmistracionPublicaeInformatica9', 1, 'Â¿Tercera pregunta booleana?', '1', '2', '3', '4', 'C'),
+('448900IAAJ9312011A0AdmistracionPublicaeInformatica9', 2, 'Â¿novena pregunta mÃºltiple?', '1', '2', '3', '4', 'D'),
+('683258IAAJ9312011A0SeminariodeMultimediaII9', 1, 'Â¿DÃ©cima pregunta mÃºltiple?', 'uno', 'dos', 'tres', 'cuatro', 'A'),
+('692823IAAJ9312011A0AdmistracionPublicaeInformatica9', 2, 'Â¿Octava pregunta mÃºltiple?', 'uno', 'dos', 'tres', 'cuatro', 'C'),
+('918115IAAJ9312011A0AdmistracionPublicaeInformatica9', 2, 'Â¿SÃ©ptima pregunta MÃºltiple?', '1', '2', '3', '4', 'B');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tableOpenQuestions`
+-- Estructura de tabla para la tabla `tableOpenQuestions`
 --
 
-CREATE TABLE IF NOT EXISTS `tableOpenQuestions` (
+CREATE TABLE `tableOpenQuestions` (
   `vcIdQuestion` varchar(60) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
   `intParcial` int(10) NOT NULL,
   `ltQuestion` longtext CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
@@ -82,64 +93,75 @@ CREATE TABLE IF NOT EXISTS `tableOpenQuestions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='This table contains the Open questions and its answers';
 
 --
--- Dumping data for table `tableOpenQuestions`
+-- Volcado de datos para la tabla `tableOpenQuestions`
 --
 
 INSERT INTO `tableOpenQuestions` (`vcIdQuestion`, `intParcial`, `ltQuestion`, `ltAnswer`) VALUES
-('13389IAAJ9312011A0QuimicaMatematicas2do3', 1, 'pregunta abierta quimica', 'respuesta pregunta abierta'),
-('656495IAAJ9312011A0AdministracionEmpresasII4', 2, 'Pregunta Prueba', 'Ponchiri');
+('20851IAAJ9312011A0AdmistracionPublicaeInformatica9', 2, 'Â¿Quinta pregunta abierta?', 'cinco'),
+('263850IAAJ9312011A0AdmistracionPublicaeInformatica9', 1, 'Â¿primera pregunta mÃºltiple?', 'uno'),
+('631146IAAJ9312011A0AdmistracionPublicaeInformatica9', 2, 'Â¿segunda pregunta abierta?', 'dos'),
+('637349IAAJ9312011A0AdmistracionPublicaeInformatica9', 2, 'Â¿octava pregunta abierta=?', 'ocho'),
+('66551IAAJ9312011A0AdmistracionPublicaeInformatica9', 1, 'Â¿SÃ©ptima pregunta abierta?', 'siete'),
+('685643IAAJ9312011A0AdmistracionPublicaeInformatica9', 2, 'Â¿DÃ©cima pregunta abierta?', 'diez'),
+('716071IAAJ9312011A0AdmistracionPublicaeInformatica9', 2, 'Â¿sexta pregunta abierta?', 'seis'),
+('808350IAAJ9312011A0AdmistracionPublicaeInformatica9', 1, 'Â¿tercera pregunta abierta?', 'tres'),
+('880917IAAJ9312011A0AdmistracionPublicaeInformatica9', 1, 'Â¿novena pregunta abierta?', 'nueve'),
+('900871IAAJ9312011A0AdmistracionPublicaeInformatica9', 2, 'Â¿Cuarta pregunta abierta?', 'cuatro');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tableQuestions`
+-- Estructura de tabla para la tabla `tableQuestions`
 --
 
-CREATE TABLE IF NOT EXISTS `tableQuestions` (
+CREATE TABLE `tableQuestions` (
   `bintQuestionIndex` bigint(20) NOT NULL,
   `vcRFC` varchar(13) NOT NULL,
   `vcIdSubject` varchar(60) NOT NULL,
-  `vcIdQuestion` varchar(60) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+  `vcIdQuestion` varchar(60) NOT NULL,
+  `intParcial` int(10) NOT NULL COMMENT 'Colum to get the partial of the exam'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tableQuestions`
+-- Volcado de datos para la tabla `tableQuestions`
 --
 
-INSERT INTO `tableQuestions` (`bintQuestionIndex`, `vcRFC`, `vcIdSubject`, `vcIdQuestion`) VALUES
-(1, 'IAAJ9312011A0', 'IAAJ9312011A0IntroduccionalasBasesdeDatos5', 'IAAJ9312011A0IntroduccionalasBasesdeDatos568660'),
-(2, 'IAAJ9312011A0', 'IAAJ9312011A0AdministracionEmpresasII4', 'IAAJ9312011A0AdministracionEmpresasII4725613'),
-(3, 'IAAJ9312011A0', 'IAAJ9312011A0IntroduccionalasBasesdeDatos5', 'IAAJ9312011A0IntroduccionalasBasesdeDatos5756676'),
-(4, 'IAAJ9312011A0', 'IAAJ9312011A0QuimicaMatematicas2do3', 'IAAJ9312011A0QuimicaMatematicas2do3247996'),
-(5, 'IAAJ9312011A0', 'IAAJ9312011A0Contabilidad II2', 'IAAJ9312011A0Contabilidad II2452940'),
-(6, 'IAAJ9312011A0', '', ''),
-(7, 'IAAJ9312011A0', '', ''),
-(8, 'IAAJ9312011A0', 'IAAJ9312011A0Multimedia II9', 'IAAJ9312011A0Multimedia II9139109'),
-(9, 'IAAJ9312011A0', 'IAAJ9312011A0QuimicaMatematicas2do3', 'IAAJ9312011A0QuimicaMatematicas2do3249707'),
-(10, 'IAAJ9312011A0', '', ''),
-(11, 'IAAJ9312011A0', 'IAAJ9312011A0Contabilidad II2', 'IAAJ9312011A0Contabilidad II2700634'),
-(12, 'IAAJ9312011A0', 'IAAJ9312011A0IntroduccionalasBasesdeDatos2', 'IAAJ9312011A0IntroduccionalasBasesdeDatos2334151'),
-(13, 'IAAJ9312011A0', 'IAAJ9312011A05', 'IAAJ9312011A05371640'),
-(14, 'IAAJ9312011A0', 'IAAJ9312011A0CÃ¡lculo Diferencial e Integral I3', 'IAAJ9312011A0CÃ¡lculo Diferencial e Integral I3122260'),
-(15, 'IAAJ9312011A0', 'IAAJ9312011A0CÃ¡lculo Diferencial e Integral I3', 'IAAJ9312011A0CÃ¡lculo Diferencial e Integral I3636745'),
-(16, 'IAAJ9312011A0', 'IAAJ9312011A05', 'IAAJ9312011A05963917'),
-(17, 'IAAJ9312011A0', 'IAAJ9312011A0CalculoDiferencialeIntegralII5', 'IAAJ9312011A0CalculoDiferencialeIntegralII573049'),
-(18, 'IAAJ9312011A0', 'IAAJ9312011A0AdministracionEmpresasII4', '656495IAAJ9312011A0AdministracionEmpresasII4'),
-(19, 'IAAJ9312011A0', 'IAAJ9312011A0QuimicaMatematicas2do3', '13389IAAJ9312011A0QuimicaMatematicas2do3'),
-(20, 'IAAJ9312011A0', 'IAAJ9312011A0Cálculo Diferencial e Integral I3', '335985IAAJ9312011A0Cálculo Diferencial e Integral I3'),
-(21, 'IAAJ9312011A0', 'IAAJ9312011A05', '149904IAAJ9312011A05'),
-(22, 'IAAJ9312011A0', 'IAAJ9312011A0IntroduccionalasBasesdeDatos2', '213487IAAJ9312011A0IntroduccionalasBasesdeDatos2'),
-(23, 'IAAJ9312011A0', 'IAAJ9312011A0CÃ¡lculo Diferencial e Integral I3', '335985IAAJ9312011A0CÃ¡lculo Diferencial e Integral I3'),
-(24, 'IAAJ9312011A0', 'IAAJ9312011A0Contabilidad II2', '318626IAAJ9312011A0Contabilidad II2'),
-(25, 'IAAJ9312011A0', 'IAAJ9312011A0QuimicaMatematicas2do3', '233585IAAJ9312011A0QuimicaMatematicas2do3');
+INSERT INTO `tableQuestions` (`bintQuestionIndex`, `vcRFC`, `vcIdSubject`, `vcIdQuestion`, `intParcial`) VALUES
+(1, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '700956IAAJ9312011A0AdmistracionPublicaeInformatica9', 1),
+(2, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '764667IAAJ9312011A0AdmistracionPublicaeInformatica9', 1),
+(3, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '543374IAAJ9312011A0AdmistracionPublicaeInformatica9', 2),
+(4, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '945662IAAJ9312011A0AdmistracionPublicaeInformatica9', 2),
+(5, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '803768IAAJ9312011A0AdmistracionPublicaeInformatica9', 1),
+(6, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '740894IAAJ9312011A0AdmistracionPublicaeInformatica9', 1),
+(7, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '539343IAAJ9312011A0AdmistracionPublicaeInformatica9', 2),
+(8, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '869875IAAJ9312011A0AdmistracionPublicaeInformatica9', 2),
+(9, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '416905IAAJ9312011A0AdmistracionPublicaeInformatica9', 1),
+(10, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '212085IAAJ9312011A0AdmistracionPublicaeInformatica9', 2),
+(11, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '447985IAAJ9312011A0AdmistracionPublicaeInformatica9', 1),
+(12, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '121525IAAJ9312011A0AdmistracionPublicaeInformatica9', 1),
+(13, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '116260IAAJ9312011A0AdmistracionPublicaeInformatica9', 2),
+(14, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '918115IAAJ9312011A0AdmistracionPublicaeInformatica9', 2),
+(15, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '692823IAAJ9312011A0AdmistracionPublicaeInformatica9', 2),
+(16, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '448900IAAJ9312011A0AdmistracionPublicaeInformatica9', 2),
+(17, 'IAAJ9312011A0', 'IAAJ9312011A0SeminariodeMultimediaII9', '683258IAAJ9312011A0SeminariodeMultimediaII9', 1),
+(18, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '263850IAAJ9312011A0AdmistracionPublicaeInformatica9', 1),
+(19, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '631146IAAJ9312011A0AdmistracionPublicaeInformatica9', 2),
+(20, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '808350IAAJ9312011A0AdmistracionPublicaeInformatica9', 1),
+(21, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '900871IAAJ9312011A0AdmistracionPublicaeInformatica9', 2),
+(22, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '20851IAAJ9312011A0AdmistracionPublicaeInformatica9', 2),
+(23, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '716071IAAJ9312011A0AdmistracionPublicaeInformatica9', 2),
+(24, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '66551IAAJ9312011A0AdmistracionPublicaeInformatica9', 1),
+(25, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '637349IAAJ9312011A0AdmistracionPublicaeInformatica9', 2),
+(26, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '880917IAAJ9312011A0AdmistracionPublicaeInformatica9', 1),
+(27, 'IAAJ9312011A0', 'IAAJ9312011A0AdmistracionPublicaeInformatica9', '685643IAAJ9312011A0AdmistracionPublicaeInformatica9', 2);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tableSubjects`
+-- Estructura de tabla para la tabla `tableSubjects`
 --
 
-CREATE TABLE IF NOT EXISTS `tableSubjects` (
+CREATE TABLE `tableSubjects` (
   `vcIdSubject` varchar(60) NOT NULL,
   `vcRFC` varchar(13) NOT NULL,
   `vcSubjectName` varchar(60) NOT NULL,
@@ -149,28 +171,25 @@ CREATE TABLE IF NOT EXISTS `tableSubjects` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tableSubjects`
+-- Volcado de datos para la tabla `tableSubjects`
 --
 
 INSERT INTO `tableSubjects` (`vcIdSubject`, `vcRFC`, `vcSubjectName`, `vcSubjectCareer`, `intTurn`, `intSemester`) VALUES
-('IAAJ9312011A0InformÃ¡tica 11', 'IAAJ9312011A0', 'InformÃ¡tica 1', 'InformÃ¡tica', 1, 1),
-('IAAJ9312011A0Multimedia II9', 'IAAJ9312011A0', 'Multimedia II', 'InformÃ¡tica', 2, 9),
-('IAAJ9312011A0Contabilidad II2', 'IAAJ9312011A0', 'Contabilidad II', 'InformÃ¡tica', 2, 2),
-('IAAJ9312011A0CÃ¡lculo Diferencial e Integral I3', 'IAAJ9312011A0', 'CÃ¡lculo Diferencial e Integral I', 'ITSE', 1, 3),
-('IAAJ9312011A05', 'IAAJ9312011A0', 'CÃ¡lculo Diferencial e Integral II', 'ITSE', 2, 5),
-('IAAJ9312011A0QuimicaMatematicas2do3', 'IAAJ9312011A0', 'QuÃ­mica MatemÃ¡ticas 2do', 'ITSE', 2, 3),
-('IAAJ9312011A0AdministracionEmpresasII4', 'IAAJ9312011A0', 'AdministraciÃ³n EmpresasII ', 'IME', 2, 4),
-('IAAJ9312011A0IntroduccionalasBasesdeDatos2', 'IAAJ9312011A0', 'IntroducciÃ³n a las Bases de Datos', 'InformÃ¡tica', 2, 2),
-('IAAJ9312011A0IntroduccionalasBasesdeDatos5', 'IAAJ9312011A0', 'IntroducciÃ³n a las Bases de Datos', 'ITSE', 2, 5),
-('IAAJ9312011A0CalculoDiferencialeIntegralII5', 'IAAJ9312011A0', 'CÃ¡lculo Diferencial e Integral II', 'ITSE', 2, 5);
+('IAAJ9312011A0AdmistracionPublicaeInformatica9', 'IAAJ9312011A0', 'AdmistraciÃ³n PÃºblica e InformÃ¡tica', 'InformÃ¡tica', 2, 9),
+('IAAJ9312011A0SeminariodeRedesII9', 'IAAJ9312011A0', 'Seminario de Redes II', 'InformÃ¡tica', 2, 9),
+('IAAJ9312011A0SeminariodeMultimediaII9', 'IAAJ9312011A0', 'Seminario de Multimedia II', 'InformÃ¡tica', 2, 9),
+('IAAJ9312011A0SeminariodeDesarrolloWebII9', 'IAAJ9312011A0', 'Seminario de Desarrollo Web II', 'InformÃ¡tica', 2, 9),
+('IAAJ9312011A0InformaticaI5', 'IAAJ9312011A0', 'InformÃ¡tica I', 'ITSE', 1, 5),
+('IAAJ9312011A0CalculoDiferencialeIntegralI7', 'IAAJ9312011A0', 'CÃ¡lculo Diferencial e Integral I', 'IME', 1, 7),
+('IAAJ9312011A0CalculoDiferencialeIntegral22', 'IAAJ9312011A0', 'CÃ¡lculo Diferencial e Integral 2', 'ITSE', 3, 2);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tableUsers`
+-- Estructura de tabla para la tabla `tableUsers`
 --
 
-CREATE TABLE IF NOT EXISTS `tableUsers` (
+CREATE TABLE `tableUsers` (
   `vcRFC` varchar(13) NOT NULL,
   `vcName` varchar(60) NOT NULL,
   `vcApellidos` varchar(60) NOT NULL,
@@ -181,7 +200,7 @@ CREATE TABLE IF NOT EXISTS `tableUsers` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tableUsers`
+-- Volcado de datos para la tabla `tableUsers`
 --
 
 INSERT INTO `tableUsers` (`vcRFC`, `vcName`, `vcApellidos`, `vcPasswd`, `vcCareer`, `intTurn`, `bType`) VALUES
@@ -189,42 +208,42 @@ INSERT INTO `tableUsers` (`vcRFC`, `vcName`, `vcApellidos`, `vcPasswd`, `vcCaree
 ('IAAJ9312011A0', 'Juan Francisco', 'Islas Aguilar', 'dwarfest', 'InformÃ¡tica', 2, 1);
 
 --
--- Indexes for dumped tables
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `tableBooleanQuestions`
+-- Indices de la tabla `tableBooleanQuestions`
 --
 ALTER TABLE `tableBooleanQuestions`
   ADD PRIMARY KEY (`vcIdQuestion`);
 
 --
--- Indexes for table `tableMultipleQuestions`
+-- Indices de la tabla `tableMultipleQuestions`
 --
 ALTER TABLE `tableMultipleQuestions`
   ADD PRIMARY KEY (`vcIdQuestion`);
 
 --
--- Indexes for table `tableOpenQuestions`
+-- Indices de la tabla `tableOpenQuestions`
 --
 ALTER TABLE `tableOpenQuestions`
   ADD PRIMARY KEY (`vcIdQuestion`);
 
 --
--- Indexes for table `tableQuestions`
+-- Indices de la tabla `tableQuestions`
 --
 ALTER TABLE `tableQuestions`
   ADD KEY `bintQuestionIndex` (`bintQuestionIndex`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `tableQuestions`
+-- AUTO_INCREMENT de la tabla `tableQuestions`
 --
 ALTER TABLE `tableQuestions`
-  MODIFY `bintQuestionIndex` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=26;
+  MODIFY `bintQuestionIndex` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
